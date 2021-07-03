a0bde7f3abbf5b515c6f729614ead95b95e16a78
tokoi <tokoi@wakayama-u.ac.jp>
2021-07-03 20:22:38 +0900

    update gg.cpp

---
+++ GgApp.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+//
+// ゲームグラフィックス特論宿題アプリケーションクラス
+//
+
+// Oculus Rift を使う
+//#define USE_OCULUS_RIFT
+
+// Dear ImGui を使う
+#define USE_IMGUI
+
+// Window 関連の処理
+#include "Window.h"
+
+class GgApp
+{
+public:
+
+  //
+  // アプリケーション本体
+  //
+  void main(int argc, const char* const* argv);
+};
